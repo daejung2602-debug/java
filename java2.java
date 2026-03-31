ce186363f1daf13b3c81d228c5aeea166d020253
daejung2602-debug <daejung2602@dge.go.kr>
2026-03-31 10:29:04 +0900

    Add files via upload

---
+++ java2.java
@@ -0,0 +1,12 @@
+package jump2java;
+
+public class test {
+	public static void main(String[] args) {
+		int i = 0;
+		System.out.println(i);
+		System.out.println(i++);
+		System.out.println(i);
+		System.out.println(++i);
+	}
+
+}
