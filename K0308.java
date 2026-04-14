1a7e1c8547006fa7ba4713ce11d72ee280766c2e
daejung2602-debug <daejung2602@dge.go.kr>
2026-04-14 09:40:10 +0900

    Add files via upload

---
+++ K0308.java
@@ -0,0 +1,10 @@
+package jumptojava;
+import java.util.HashMap;
+public class K0308 {
+	public static void main(String[] arg) {
+		HashMap<String, String> map = new HashMap<>();
+		map.put("pepole", "사람");
+		map.put("baseball", "야구");
+		System.out.println(map.keySet());
+	}
+}
